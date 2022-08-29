9136e3589e2b32f3142c8e05493bc6d1c536cb51
EZaiger <elisabeth.zaiger@oit.edu>
2022-08-29 14:10:12 -0700

    general file setup and database info entry

---
+++ db/seeds.sql
@@ -0,0 +1,13 @@
+
+
+INSERT INTO employee
+    (first_name, last_name, role_id, manager_id)
+VALUES
+    ('John', 'Doe', ),
+    ('Mike', 'Chan'),
+    ('Ashley', 'Rodriguez'),
+    ('Kevin', 'Tupik'),
+    ('Kunal', 'Singh'),
+    ('Malia', 'Brown'),
+    ('Sarah', 'Lourd'),
+    ('Tom', 'Allen');
